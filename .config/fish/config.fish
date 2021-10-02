7d526555343cb7e1cbb159eae8844018fc8e8769
Theo <theo.emeriau@gmail.com>
2021-10-02 10:49:37 +0200

    fish ls to exa

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,15 @@
 set PATH $HOME/.cargo/bin $PATH
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+alias vim='nvim'
+alias fd='fdfind'
+
+if command -v exa > /dev/null
+	abbr -a l 'exa'
+	abbr -a ls 'exa'
+	abbr -a ll 'exa -l'
+	abbr -a lll 'exa -la'
+else
+	abbr -a l 'ls'
+	abbr -a ll 'ls -l'
+	abbr -a lll 'ls -la'
+end
